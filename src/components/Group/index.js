7cc47683e7143344acaac9c71fd6a5c8fa2f64fc
Dylan Jones <dyl@anjon.es>
2020-04-11 17:53:49 +0100

    Remove basic component and use media cards to link to groups

--- src/components/Group/index.js
+++ src/components/Group/index.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { withFirebase } from '../Firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 function Group(props) {
 
   const { uid } = useParams();
   const [group, setGroup] = useState({});
+  const classes = useStyles();
 
   useEffect(() => {
     const unsubscribe = props.firebase
@@ -18,7 +27,22 @@ function Group(props) {
 
   return (
     <div>
-      <h1>Group: {group.title}</h1>
+      <h1>{group.title}</h1>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={8}>
+            <h3>Chat</h3>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <h3>Members</h3>
+            <p>Bob</p>
+            <p>John</p>
+            <h3>Stories</h3>
+            <p>Bob</p>
+            <p>John</p>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 
